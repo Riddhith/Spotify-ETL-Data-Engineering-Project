5de2750c5f0a422d807a4b4d90733082bdb9100c
Riddhith Banerjee <riddhith04@gmail.com>
2025-01-19 23:02:54 +0530

    adding all the files.

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+ck_data = {
+    'Track Name': track['name'],
+    'Artist': track['artists'][0]['name'],
+    'Album': track['album']['name'],
+    'Popularity': track['popularity'],
+    'Duration (minutes)': track['duration_ms'] /60000
+}
